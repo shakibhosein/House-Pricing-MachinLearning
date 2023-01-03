4c5e39f7feea3d46899b1335ebfda2500f3a65a2
Hosein Shakib <shakibhoseinkh@gmail.com>
2023-01-02 16:34:35 -0800

    Making almost all the neural network related lines ineffective to test the code in Ubuntu which has no access to GPU.

--- HousePrices.py
+++ HousePrices.py
@@ -1,55 +1,57 @@
 #---------------------------------------- Importing Libraries
 from pandas import read_excel
-from keras.models import Sequential
-from keras.layers import Dense, activation
+# from keras.models import Sequential
+# from keras.layers import Dense, activation
 import numpy
 # import os
-# os.system("cls")
+# os.system("clear")
 #-----------------------------------------Reading Excel file
+Dataset_directory = "/home/hosein/Desktop/HousePricing-MachinLearning/Assignment 2_BUSI 651_House Prices.xlsx"
 
-dataset = read_excel("C:/Users/shaki/OneDrive/Desktop/UCW/Third Term/BUSI 651 - Machine Learning Tools and Techniques/Assignment 2 - Team Work/Assignment 2_BUSI 651_House Prices.xlsx")
+dataset = read_excel(Dataset_directory)
 datalength = len(dataset)
+print(datalength)
 
-#-----------------------------------------deviding dataset into Y and X datasets
+# #-----------------------------------------deviding dataset into Y and X datasets
 
-x_data = dataset[["GarageCars","GarageArea","OverallQual","GrLivArea"]]
-y_data = dataset["SalePrice"]
+# x_data = dataset[["GarageCars","GarageArea","OverallQual","GrLivArea"]]
+# y_data = dataset["SalePrice"]
 
 
-#-----------------------------------------deviding X and Y datasets into train and test datasets
+# #-----------------------------------------deviding X and Y datasets into train and test datasets
 
-train_len=int(datalength*0.8)
-x_train=x_data[:train_len+1]
-y_train=y_data[:train_len+1]
-x_test=x_data[train_len+1:]
-y_test=y_data[train_len+1:]
+# train_len=int(datalength*0.8)
+# x_train=x_data[:train_len+1]
+# y_train=y_data[:train_len+1]
+# x_test=x_data[train_len+1:]
+# y_test=y_data[train_len+1:]
 
-#------------------------------------------Design Model
+# #------------------------------------------Design Model
 
-price_model = Sequential()
-price_model.add(Dense(64,activation = "relu",input_dim = 4)) #first hidden layer with 4 input
-price_model.add(Dense(64,activation="relu"))#Second hidden layer
-price_model.add(Dense(64,activation="relu"))#Third hidden layer
-price_model.add(Dense(1))
+# price_model = Sequential()
+# price_model.add(Dense(64,activation = "relu",input_dim = 4)) #first hidden layer with 4 input
+# price_model.add(Dense(64,activation="relu"))#Second hidden layer
+# price_model.add(Dense(64,activation="relu"))#Third hidden layer
+# price_model.add(Dense(1))
 
-#-----------------------------------------Compile and fit model
+# #-----------------------------------------Compile and fit model
 
-price_model.compile(loss="mean_squared_error",optimizer="adam")
-price_model.fit(x_train,y_train,epochs=1000)
+# price_model.compile(loss="mean_squared_error",optimizer="adam")
+# price_model.fit(x_train,y_train,epochs=1000)
 
-#-----------------------------------------Testing the model
+# #-----------------------------------------Testing the model
 
-y_predict = price_model.predict(x_test)
+# y_predict = price_model.predict(x_test)
 
-#-----------------------------------------reshaping
+# #-----------------------------------------reshaping
 
-y_predicted_1 = y_predict.reshape(1,datalength-train_len-1)
-y_test_1 = y_test.to_numpy()
-APE = abs( numpy.subtract(y_predicted_1, y_test_1) )/y_test_1*100
-MAPE = numpy.mean(APE)
-print(MAPE)
-#-----------------------------------------Prediction
-# print(price_model.predict([[2,600,7,2200]]))
+# y_predicted_1 = y_predict.reshape(1,datalength-train_len-1)
+# y_test_1 = y_test.to_numpy()
+# APE = abs( numpy.subtract(y_predicted_1, y_test_1) )/y_test_1*100
+# MAPE = numpy.mean(APE)
+# print(MAPE)
+# #-----------------------------------------Prediction
+# # print(price_model.predict([[2,600,7,2200]]))
 
 
 
